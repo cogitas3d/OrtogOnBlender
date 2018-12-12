57ec91f75aba50cbe7cdaeeefd00bc61f0d8c827
Cicero Moraes <cogitas3d@gmail.com>
2018-12-12 10:20:19 -0200

    Add files via upload
    Fix Windows upgrade error

--- AtualizaScript.py
+++ AtualizaScript.py
@@ -5,4 +5,15 @@ import platform
 def AtualizaScriptDef(self, context):
 
 	if platform.system() == "Windows":
-		subprocess.call('cd C:\OrtogOnBlender\Blender\2.78\scripts\addons\OrtogOnBlender-master && atualiza_ortog.bat', shell=True)
+#		subprocess.call('cd C:\OrtogOnBlender\Blender\2.78\scripts\addons\OrtogOnBlender-master && atualiza_ortog.bat', shell=True)
+
+		arquivo = open('atualiza_ortog.bat', 'w+')
+		arquivo.writelines("""cd C:/OrtogOnBlender/Blender/2.78/scripts/addons && ^
+rd /s /q OrtogOnBlender-master && ^
+C:/OrtogOnBlender/Python27/python.exe -c "import urllib; urllib.urlretrieve ('https://github.com/cogitas3d/OrtogOnBlender/archive/master.zip', 'master.zip')" && ^
+C:/OrtogOnBlender/7-Zip/7z x  master.zip && ^
+del master.zip""")
+#faca o que quiser
+		arquivo.close()
+
+		subprocess.call('atualiza_ortog.bat', shell=True)
