8088a3377e7a8a23055a8a4c9d246edcff1cb715
Cicero Moraes <cogitas3d@gmail.com>
2019-03-01 13:21:03 -0400

    Draw Line Cut - all operation systems!

--- DesenhaGuia.py
+++ DesenhaGuia.py
@@ -157,7 +157,7 @@ def DesenhaLinhaCorteDef(self, context):
 
 # Seleciona o desenho
 
-    bpy.ops.gpencil.convert(type='PATH')
+    bpy.ops.gpencil.convert(type='CURVE', timing_mode='LINEAR', use_timing_data=False)
     bpy.ops.gpencil.layer_remove()
 
     Linha = bpy.data.objects['GP_Layer']
@@ -169,36 +169,47 @@ def DesenhaLinhaCorteDef(self, context):
 
     bpy.ops.object.mode_set(mode='EDIT')
     bpy.ops.curve.make_segment()
+    bpy.ops.curve.select_all(action='TOGGLE')
+    bpy.ops.curve.select_all(action='TOGGLE')
+    bpy.ops.curve.handle_type_set(type='AUTOMATIC')
     bpy.ops.object.mode_set(mode='OBJECT')
+
+    bpy.ops.object.modifier_add(type='SHRINKWRAP')
+    bpy.context.object.modifiers["Shrinkwrap"].use_keep_above_surface = True
+    bpy.context.object.modifiers["Shrinkwrap"].target = Osso
+    bpy.context.object.modifiers["Shrinkwrap"].offset = 2
+    bpy.ops.object.modifier_add(type='SMOOTH')
+    bpy.context.object.modifiers["Smooth"].factor = 2
+    bpy.context.object.modifiers["Smooth"].iterations = 3
     bpy.ops.object.convert(target='MESH')
-    bpy.ops.object.mode_set(mode='EDIT')
-    bpy.ops.mesh.select_all(action='TOGGLE')
-
-    bpy.ops.mesh.offset_edges(caches_valid=True, width=2.2, flip_width=False, depth=0, flip_depth=False, angle=0, flip_angle=False)
-    bpy.ops.mesh.select_all(action='INVERT')
-    bpy.ops.mesh.delete(type='VERT')
-
+    bpy.ops.object.mode_set(mode='EDIT')    
     bpy.ops.mesh.select_all(action='TOGGLE')
     bpy.ops.mesh.remove_doubles()
-    bpy.ops.mesh.remove_doubles(threshold=1)
-
+    bpy.ops.mesh.remove_doubles(threshold=2)
     bpy.ops.mesh.fill()
-    bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":(0, 0, 0.3), "constraint_axis":(False, False, True), "constraint_orientation":'NORMAL', "mirror":False, "proportional":'DISABLED', "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False})
+    bpy.ops.mesh.extrude_region()
+    bpy.ops.mesh.extrude_region_move(MESH_OT_extrude_region={"mirror":False}, TRANSFORM_OT_translate={"value":(3.72529e-09, 7.91624e-09, 0.3), "constraint_axis":(False, False, True), "constraint_orientation":'NORMAL', "mirror":False, "proportional":'DISABLED', "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False})
 
-
-    bpy.ops.object.mode_set(mode='OBJECT')
-
+    bpy.ops.object.editmode_toggle()
+ 
     bpy.context.object.name = "Corte"    
     Linha = bpy.data.objects['Corte']
 
     bpy.ops.object.select_all(action='DESELECT')
     Linha.select = True
     Osso.select = True
-    bpy.context.scene.objects.active = Linha
+    bpy.context.scene.objects.active = Osso
 
+# Se não duplicar não funciona!
+    bpy.ops.object.duplicate()
 
     bpy.ops.object.booleana_osteo()
-    
+ 
+# Apaga os objetos anteriores   
+    bpy.ops.object.select_all(action='DESELECT')
+    Linha.select = True
+    Osso.select = True
+    bpy.ops.object.delete(use_global=False)
 
 
 
