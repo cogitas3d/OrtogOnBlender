b6b4189bf80a72869fb953a1ae2f661a921e559b
Cicero Moraes <cogitas3d@gmail.com>
2019-08-25 14:41:48 -0400

    ForensicOnBlender

--- __init__.py
+++ __init__.py
@@ -75,7 +75,7 @@ class ORTOG_PT_AtualizaAddonSec(bpy.types.Panel):
         scn = context.scene
 
         row = layout.row()
-        row.label(text="VERSION: 20190825a")
+        row.label(text="VERSION: 20190825b")
 
         row = layout.row()
         row.operator("object.atualiza_script", text="UPGRADE ORTOG!", icon="RECOVER_LAST")
@@ -2448,6 +2448,124 @@ def unregister():
     bpy.utils.unregister_class(ORTOG_PT_GuideCreation)
     bpy.utils.unregister_class(ORTOG_PT_ImportTomoImg)
 
-        
+
+class FORENSIC_PT_Fotogrametria(bpy.types.Panel):
+    bl_label = "Photogrammetry Start"
+    bl_region_type = 'UI'
+    bl_space_type = 'VIEW_3D'
+    bl_options = {'DEFAULT_CLOSED'}
+    bl_category = "Forensic"
+
+    def draw(self, context):
+        layout = self.layout
+
+        context = bpy.context
+        obj = context.object
+        scn = context.scene
+
+        row = layout.row()
+        row.label(text="OpenMVG+OpenMVS:")
+
+        row = layout.row()
+        col = layout.column(align=True)
+        col.prop(scn.my_tool, "path", text="")
+
+        col = self.layout.column(align = True)
+        col.alignment = 'RIGHT'
+        col.prop(context.scene, "d_factor")
+        col.prop(context.scene, "smooth_factor")
+
+        if platform.system() == "Windows":
+            row = layout.row()
+            row.operator("wm.console_toggle", text="Open Terminal?", icon="CONSOLE")
+			
+        row = layout.row()
+        row.operator("object.gera_modelo_foto", text="Start Photogrammetry!", icon="IMAGE_DATA")
+
+bpy.utils.register_class(FORENSIC_PT_Fotogrametria)
+
+class FORENSIC_PT_AlinhaFace(bpy.types.Panel):
+    bl_label = "Photogrammetry - Align & Scale"
+    bl_region_type = 'UI'
+    bl_space_type = 'VIEW_3D'
+    bl_options = {'DEFAULT_CLOSED'}
+    bl_category = "Forensic"
+
+    def draw(self, context):
+        layout = self.layout
+
+        context = bpy.context
+        obj = context.object
+        scn = context.scene
+
+        row = layout.row()
+        row.label(text="Mode:")
+
+        row = layout.row()
+        linha=row.operator("wm.tool_set_by_id", text="Cursor", icon="PIVOT_CURSOR").name="builtin.cursor"
+
+        row = layout.row()
+        linha=row.operator("wm.tool_set_by_id", text="Select", icon="RESTRICT_SELECT_OFF").name="builtin.select_box"
+
+        row = layout.row()
+        row = layout.row()
+        row.label(text="Align Points:")
+
+        row = layout.row()
+        linha=row.operator("object.emp1b", text="Cantal Lateral Right", icon="SORTBYEXT")
+
+        row = layout.row()
+        linha=row.operator("object.emp2b", text="Cantal Lateral Left", icon="SORTBYEXT")
+
+        row = layout.row()
+        linha=row.operator("object.emp3b", text="Down Point", icon="SORTBYEXT")
+
+#        row = layout.row()
+#        row.operator("object.cria_tres_pontos", text="3 Points Click", icon="OUTLINER_OB_MESH")
+
+        col = self.layout.column(align = True)
+        col.prop(context.scene, "medida_real2")  
+
+        row = layout.row()
+        row.operator("object.alinha_forca", text="Align and Resize!", icon="ORIENTATION_LOCAL")
+
+        row = layout.row()
+        row = layout.row()
+        row.label(text="Segmentation Cut Through:")
+
+        row = layout.row()
+        row.operator("gpencil.annotate", icon='LINE_DATA', text="Draw Line").mode = 'DRAW_POLY'
+
+        row = layout.row()
+        linha=row.operator("object.segmenta_desenho", text="Cut Draw!", icon="FCURVE")
+
+
+        row = layout.row()
+        row = layout.row()
+        row.label(text="Surface Cut:")
+
+        row = layout.row()
+        row.operator("wm.modal_cria_pontos", icon='CURVE_DATA', text="Create Points")
+
+        row = layout.row()
+        row.operator("mesh.add_curva_bezier_unido", icon='CURVE_BEZCIRCLE', text="Create Bezier Line")
+
+        row = layout.row()
+        circle=row.operator("object.bezier_corta", text="Cut Line!", icon="SCULPTMODE_HLT")
+
+        row = layout.row()
+        circle=row.operator("object.bezier_corta_dupla", text="Cut Line Double!", icon="MOD_THICKNESS")
+
+        row = layout.row()
+        row = layout.row()
+        box = layout.box()
+        col = box.column(align=True)
+        row = col.row()
+        row.scale_y=1.5
+        row.alignment = 'CENTER'
+        row.operator("object.gera_dir_nome_paciente_alinha_face", text="SAVE!", icon="FILE_TICK")      
+
+bpy.utils.register_class(FORENSIC_PT_AlinhaFace)
+
 if __name__ == "__main__":
     register()
