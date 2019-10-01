a7973c163a6136c4cbb389ecd3f169fd4d7f1c70
Cicero Moraes <cogitas3d@gmail.com>
2019-10-01 08:52:23 -0300

    CT-Scan database.

--- GeraModelosTomo.py
+++ GeraModelosTomo.py
@@ -747,32 +747,32 @@ def IdentificaTomografo(Arquivo):
 
         bpy.ops.object.gera_modelos_tomo()
 
-if ManufacturerLimpo == "'TOSHIBA'" and ManufacturerModelNameLimpo == "'Aquilion PRIME'":
-    print("USA FIXED!")
-    print("SÉRIE 5")
-    print("Bone: 200")
-    print("SoftTissue: -300")
-    print("Teeth: 1430")
-    print("Condylus: 655")
+    if ManufacturerLimpo == "'TOSHIBA'" and ManufacturerModelNameLimpo == "'Aquilion PRIME'":
+        print("USA FIXED!")
+        print("SÉRIE 5")
+        print("Bone: 200")
+        print("SoftTissue: -300")
+        print("Teeth: 1430")
+        print("Condylus: 655")
 
-    os.chdir(scn.my_tool.path+"/5")
-    scn.my_tool.path = os.getcwd()
-    bpy.ops.object.corrige_dicom()
+        os.chdir(scn.my_tool.path+"/5")
+        scn.my_tool.path = os.getcwd()
+        bpy.ops.object.corrige_dicom()
 
-#        bpy.ops.object.reduz_dimensao_dicom()
+    #        bpy.ops.object.reduz_dimensao_dicom()
 
-    # Copia para o diretório
-    try:
-        CopiaTomoDir(scn.my_tool.path)
-    except:
-        print("Doesn't have Patient Dir")
+        # Copia para o diretório
+        try:
+            CopiaTomoDir(scn.my_tool.path)
+        except:
+            print("Doesn't have Patient Dir")
 
-    # Gera o 3D
-    bpy.context.scene.interesse_ossos = "200"
-    bpy.context.scene.interesse_mole = "-300"
-    bpy.context.scene.interesse_dentes = "1430"
+        # Gera o 3D
+        bpy.context.scene.interesse_ossos = "200"
+        bpy.context.scene.interesse_mole = "-300"
+        bpy.context.scene.interesse_dentes = "1430"
 
-    bpy.ops.object.gera_modelos_tomo()
+        bpy.ops.object.gera_modelos_tomo()
 
     if ManufacturerLimpo == "'TOSHIBA'" and ManufacturerModelNameLimpo == "'Aquilion'":
 
