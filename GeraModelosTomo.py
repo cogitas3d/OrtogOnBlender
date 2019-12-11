425269029dac194d3062fde79b05d1c5a0eff869
Cicero Moraes <cogitas3d@gmail.com>
2019-12-11 12:54:59 -0300

    CT-Scan database.

--- GeraModelosTomo.py
+++ GeraModelosTomo.py
@@ -1260,6 +1260,59 @@ def IdentificaTomografo(Arquivo):
 
         bpy.ops.object.gera_modelos_tomo()
 
+
+    if ManufacturerLimpo == "'SIEMENS'" and StationNameLimpo == "'CT80781'" and ManufacturerModelNameLimpo == "'Emotion 16 (2010)'":
+        print("SÉRIE 3")
+        print("Bone: 200")
+        print("SoftTissue: -300")
+        print("Teeth: 1430")
+        print("Condylus: 655")
+
+        os.chdir(scn.my_tool.path+"/3")
+        scn.my_tool.path = os.getcwd()
+        bpy.ops.object.corrige_dicom()
+
+#        bpy.ops.object.reduz_dimensao_dicom()
+
+        # Copia para o diretório
+        try:
+            CopiaTomoDir(scn.my_tool.path)
+        except:
+            print("Doesn't have Patient Dir")
+
+        # Gera o 3D
+        bpy.context.scene.interesse_ossos = "200"
+        bpy.context.scene.interesse_mole = "-300"
+        bpy.context.scene.interesse_dentes = "1430"
+
+        bpy.ops.object.gera_modelos_tomo()
+
+
+    if ManufacturerLimpo == "'SIEMENS'" and StationNameLimpo == "'CTAWP96599'" and ManufacturerModelNameLimpo == "'SOMATOM Definition AS+'":
+        print("SÉRIE 3")
+        print("Bone: 295")
+        print("SoftTissue: -300")
+        print("Teeth: 780")
+
+        os.chdir(scn.my_tool.path+"/3")
+        scn.my_tool.path = os.getcwd()
+        bpy.ops.object.corrige_dicom()
+
+#        bpy.ops.object.reduz_dimensao_dicom()
+
+        # Copia para o diretório
+        try:
+            CopiaTomoDir(scn.my_tool.path)
+        except:
+            print("Doesn't have Patient Dir")
+
+        # Gera o 3D
+        bpy.context.scene.interesse_ossos = "295"
+        bpy.context.scene.interesse_mole = "-300"
+        bpy.context.scene.interesse_dentes = "780"
+
+        bpy.ops.object.gera_modelos_tomo()
+
     if ManufacturerLimpo == "'SIEMENS'" and StationNameLimpo == "'CT2SQ'" and ManufacturerModelNameLimpo == "'SOMATOM Definition AS+'":
         print("SÉRIE 3")
         print("Bone: 200")
--- __init__.py
+++ __init__.py
@@ -61,7 +61,7 @@ from bpy.types import (Panel,
                        PropertyGroup,
                        )
 
-VERSION = "20191210a"
+VERSION = "20191211b"
 
 # ATUALIZA SCRIPT
 class ORTOG_PT_AtualizaAddonSec(bpy.types.Panel):
