1c01b55f19e7e1c43363227a7b350b7ae5d3d848
Cicero Moraes <cogitas3d@gmail.com>
2020-01-28 09:35:16 -0400

    CT-Scan database

--- GeraModelosTomo.py
+++ GeraModelosTomo.py
@@ -3471,6 +3471,30 @@ def IdentificaTomografo(Arquivo):
         bpy.ops.object.gera_modelos_tomo()
 
 
+    if ManufacturerLimpo == "'NewTom'" and StationNameLimpo == "'NT'" and ManufacturerModelNameLimpo == "'NTVGiMK4'":
+
+
+        # TAMBÉM FUNCIONA O 201
+        os.chdir(scn.my_tool.path+"/1")
+        scn.my_tool.path = os.getcwd()
+        bpy.ops.object.corrige_dicom()
+
+#        bpy.ops.object.reduz_dimensao_dicom()
+
+        # Copia para o diretório
+        try:
+            CopiaTomoDir(scn.my_tool.path)
+        except:
+            print("Doesn't have Patient Dir")
+
+        # Gera o 3D
+        bpy.context.scene.interesse_ossos = "670"
+        bpy.context.scene.interesse_mole = "-450"
+        bpy.context.scene.interesse_dentes = "950"
+
+        bpy.ops.object.gera_modelos_tomo()
+
+
     if ManufacturerLimpo == "'NewTom'" and StationNameLimpo == "'NT'" and ManufacturerModelNameLimpo == "'NT5G'":
         print("SÉRIE 1")
         print("Bone: 520")
--- __init__.py
+++ __init__.py
@@ -62,7 +62,7 @@ from bpy.types import (Panel,
                        PropertyGroup,
                        )
 
-VERSION = "20200127a"
+VERSION = "20200128a"
 
 # ATUALIZA SCRIPT
 class ORTOG_PT_AtualizaAddonSec(bpy.types.Panel):
